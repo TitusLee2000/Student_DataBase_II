b9e339bd53d601a412bdf1d3802b4bd4dc2be7e8
Titus <ttlleegml@gmail.com>
2025-03-14 15:32:11 -0700

    Created repository

---
+++ main.c
@@ -0,0 +1,57 @@
+//
+// Created by titus on 2025-03-14.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Student{
+  char name[100];
+  int id;
+  int age;
+  char program[50];
+  float gpa;
+  char group[50];
+};
+
+
+void addStudent() {
+    //prompt and retrieve user input
+
+    //validation
+
+    //add to list
+}
+
+void DisplayStudent() {
+
+}
+
+Student searchStudent() {
+
+}
+
+void deleteStudent() {
+
+}
+
+void ListGroupMembers() {
+
+}
+//helper
+void sortbyID() {}
+void sortbyName() {}
+void sortbyGPA() {}
+//ascending
+//decending
+void sortStudent() {
+
+}
+
+void exit() {
+
+}
+
+int main() {
+
+}
